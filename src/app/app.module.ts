fb4ea2078deb9ef16f25dc1264a4df6ad31eafac
redab <reda.bakr@gmail.com>
2021-12-27 10:23:33 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {CdkDragDropEnterPredicateExample} from './cdk-drag-drop-enter-predicate-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [CdkDragDropEnterPredicateExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [CdkDragDropEnterPredicateExample],
+})
+export class AppModule {}
---
+++ src/app/cdk-drag-drop-enter-predicate-example.ts
@@ -0,0 +1,65 @@
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag
+} from '@angular/cdk/drag-drop';
+import * as _ from "lodash";
+
+interface Item {
+  value: string,
+  index: number
+}
+
+@Component({
+  selector: 'cdk-drag-drop-enter-predicate-example',
+  templateUrl: 'cdk-drag-drop-enter-predicate-example.html',
+  styleUrls: ['cdk-drag-drop-enter-predicate-example.css'],
+})
+
+export class CdkDragDropEnterPredicateExample {
+  @ViewChildren('destItems') destItems: QueryList<ElementRef>
+
+  sourceList: Item[] =[
+    {value:'A', index:1},
+    {value:'E', index:5},
+    {value:'C', index:3},
+    {value:'D', index:4},
+    {value:'F', index:6},
+    {value:'B', index:2}
+  ];
+  destinationList: Item[] =[
+    {value:'1', index:1},
+    {value:'2', index:2},
+    {value:'3', index:3},
+    {value:'4', index:4},
+    {value:'5', index:5},
+    {value:'6', index:6}
+  ];
+
+ 
+  drop(event: CdkDragDrop<Item[]>) {
+    if (event.previousContainer === event.container) { return;}
+    if(event.currentIndex >= this.destinationList.length){
+      alert("Invalid Position!");
+      return;
+    }
+    let from = event.previousContainer.data[event.previousIndex];
+    let to = event.container.data[event.currentIndex]
+    if(from.index != to.index || event.currentIndex >= this.destinationList.length){
+      alert("Invalid Position!");
+      return;
+    }
+   
+    event.container.data[event.currentIndex].value = from.value + " " + to.value;
+     _.remove(event.previousContainer.data,(item)=>item.index == from.index);
+    // update color to green
+     this.destItems.get(event.currentIndex)?.nativeElement.classList.remove('destinationBox');
+     this.destItems.get(event.currentIndex)?.nativeElement.classList.add('mergedBox');    
+  }
+  evenPredicate(item: CdkDrag<Item[]>) {
+    return true;  
+  }
+  noReturnPredicate() {
+    return false;
+  }
+}
